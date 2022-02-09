404a7b9483989dbf48c8e5df466670adccc69d66
JavohirO2 <javohir.jalolitdinov@mail.ru>
2022-02-09 15:50:12 -0500

    Initial commit

---
+++ src/ProductReader.java
@@ -0,0 +1,89 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class ProductReader
+{
+    public static void main(String[] args)
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "" ;
+
+        ArrayList<String> lines = new ArrayList<>();
+
+        try
+        {
+
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            // Typiacally, we want the user to pick the file so we use a file chooser
+            // kind of ugly code to make the chooser work with NIO.
+            // Because the chooser is part of Swing it should be thread safe.
+            chooser.setCurrentDirectory(workingDirectory);
+            // Using the chooser adds some complexity to the code.
+            // we have to code the complete program within the conditional return of
+            // the filechooser because the user can close it without picking a file
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+                // Typical java pattern of inherited classes
+                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                // Finally we can read the file LOL!
+                int line = 0;
+
+                String words[];
+
+                System.out.println("ID#        Name           Description                     Cost");
+                System.out.println("===============================================================");
+
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    lines.add(rec);
+                    line++;
+                    //System.out.println(String.format("%s%n", rec));
+                }
+
+
+
+                for(String l : lines) {
+                    String fields[] = l.split(", ");
+
+                    System.out.printf("%-10s %-15s %-30s %-20s %n",fields[0],fields[1], fields[2], fields[3]);
+
+                }
+
+                reader.close(); // must close the file to seal it and flush buffer
+                System.out.println("\n\nData file read!");
+            }
+            else  // user closed the file dialog without choosing
+            {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
+            }
+        }  // end of TRY
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
